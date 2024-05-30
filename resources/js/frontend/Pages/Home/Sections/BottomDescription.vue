e030df374507e8c3072f212e7049c0c78f3b8165
Md-shefat-masum <mshefat924@gmail.com>
2024-05-30 19:54:18 +0600

    frontend updated

---
+++ resources/js/frontend/Pages/Home/Sections/BottomDescription.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="container">
+        <div class="product_info_content mb-2" id="product_info_content">
+            <div class="product_d_inner">
+                <h1>Best Computer, Laptop, Gaming PC, PC Components, Retail &amp; Wholesale Online Shop in Bangladesh
+                </h1>
+                <h2>Best Desktop PC Shop in Bangladesh</h2>
+                <p>
+                    ETEK BD is the Best<a href="https://www.etek.com.bd/desktop-pc"> Desktop PC</a> Shop in
+                    Bangladesh, providing the best quality PCs at the most competitive prices. With 21 years of PC
+                    building experience, you can
+                    be sure that the PCs here are reliable, powerful, and cost-effective. Our knowledgeable executives
+                    are always on hand to offer expert advice and support, ensuring that you’re getting the best value
+                    for your money. Whether
+                    you’re a casual user or a professional in need of a powerhouse machine, ETEK BD is The Best
+                    Desktop PC Shop in Bangladesh is the perfect destination for all your PC needs.
+                </p>
+                <h2>Best Gaming PC Shop in Bangladesh</h2>
+                <p>
+                    If you're looking for the best <a href="https://www.etek.com.bd/gaming-pc">gaming PC </a>shop
+                    in Bangladesh, then you've come to the right place! At our store, we offer a wide selection of
+                    gaming PCs that are perfect
+                    for any type of gaming experience. Our knowledgeable staff is always here to answer any questions
+                    you may have, and help you find the perfect gaming PC that fits both your budget and gaming needs.
+                    <a href="https://www.etek.com.bd/direction">Visit us today</a> and experience the best gaming
+                    PC shop in Bangladesh.
+                </p>
+                <h2>Best Office PC Shop in Bangladesh</h2>
+                <p>
+                    If you're looking for the best<a href="https://www.etek.com.bd/binary-pc"> office PC </a>shop
+                    in Bangladesh, then you've come to the right place. At ETEK BD, we provide the best quality
+                    laptops, desktops, and
+                    accessories to get your office up and running with ease. We offer attractive prices, reliable
+                    service, and prompt delivery. Our friendly staff is always ready to answer any questions you have
+                    and provide you with helpful
+                    advice. So, come on down and see for yourself why ETEK BD is the best office PC shop in
+                    Bangladesh.
+                </p>
+                <h2>Best Graphics PC Shop in Bangladesh</h2>
+                <p>
+                    Welcome to the best <a href="https://www.etek.com.bd/graphic-pc">graphics PC shop in
+                        Bangladesh</a> at ETEK BD! Here, we make your dreams come true by providing only the best
+                    high-end components and the latest
+                    technologies. Our PCs are designed with cutting-edge graphics and performance in mind, so you can
+                    experience maximum immersion and power for the latest graphics design. Plus, our expertise in PC
+                    building lets you
+                    <a href="https://www.etek.com.bd/tools/pc_builder"> customize your PC </a>to meet your exact
+                    needs. Get ready to take your graphics designing experience to the next level at ETEK BD.
+                </p>
+                <h2>Best &nbsp;Video Editing PC Shop in Bangladesh</h2>
+                <p>
+                    ETEK BD specializes in helping customers build their ideal <a
+                        href="https://www.etek.com.bd/graphic-pc">video editing PC</a>. Our knowledgeable and
+                    experienced staff will provide you with personalized
+                    recommendations for each component to ensure your PC meets the specific needs of your video editing
+                    workflow. We’ll help you build a powerful, reliable system that is designed to allow you to produce
+                    professional-quality
+                    videos quickly and efficiently. With our Build Your Video Editing PC, we guarantee that you’ll get
+                    the best possible value for your money and be able to edit videos with ease.
+                </p>
+                <h2>Best Server PC Shop in Bangladesh. Build Your Server PC at ETEK BD.</h2>
+                <p>
+                    ETEK BD is the premier shop for<a href="https://www.etek.com.bd/tools/pc_builder">
+                        building your server PC in Bangladesh</a>. With top-of-the-line components from leading brands,
+                    custom PC building services, and
+                    knowledgeable, friendly staff, ETEK BD is the best choice for constructing your server PC. All
+                    of the components are top-notch and tested to ensure the highest quality performance for your
+                    server, and the staff can
+                    assist with any questions or concerns you may have. With the combination of quality hardware and
+                    superior customer service, ETEK BD is the premier shop for building your
+                    <a href="https://www.etek.com.bd/server">server PC</a>.<br />
+                    &nbsp;
+                </p>
+                <h2>Best PC case shop at an affordable price&nbsp;in Bangladesh</h2>
+                <p>
+                    We are the best PC case shop in Bangladesh because we provide good quality products at an affordable
+                    price. Our products are made from durable materials and are designed to protect your computers from
+                    damage. We also offer a
+                    wide variety of styles and colors to choose from <a
+                        href="https://www.etek.com.bd/darkflash">darkflash </a>&amp; <a
+                        href="https://www.etek.com.bd/cooler-master">Cooler Master</a>, so you can find the
+                    perfect
+                    <a href="https://www.etek.com.bd/case">pc case </a>for your needs.
+                </p>
+                <h2>Best Computer Motherboard shop at an affordable price in Bangladesh</h2>
+                <p>
+                    Need a new <a href="https://www.etek.com.bd/motherboard">computer motherboard</a> but don't
+                    want to spend a fortune? Check out our list of the best computer motherboards for an affordable
+                    price in Bangladesh. We've
+                    got options for every budget, so you can find the perfect motherboard for your needs. Whether you're
+                    looking for a high-end option for gaming or a budget-friendly option for work, we've got you
+                    covered.&nbsp;
+                </p>
+                <p>No matter what your needs are, we're sure you'll <a
+                        href="https://www.etek.com.bd/motherboard">find</a> the perfect computer motherboard on
+                    our website.</p>
+                <h2>Best &nbsp;Graphics Card shop at an affordable price in Bangladesh&nbsp;</h2>
+                <p>
+                    If you're looking for the best graphics card shop at an affordable price in Bangladesh, then you've
+                    come to the right place. We offer a wide range of graphics cards from leading brands, all at great
+                    prices. Our selection
+                    includes cards for both desktops and laptops, so you're sure to find the perfect card for your
+                    needs. And if you're not sure which card is right for you, our friendly and knowledgeable staff will
+                    be happy to help you choose
+                    the perfect card for your budget and needs.
+                </p>
+                <p>So come and visit us today, and see for yourself why we're the best place to buy <a
+                        href="https://www.etek.com.bd/graphics-card">graphics cards</a> in Bangladesh.</p>
+                <h2>Best POS Printer shop at an affordable price in Bangladesh</h2>
+                <p>
+                    Are you looking for a POS printer shop in Bangladesh? Look no further than Printer Point. We offer
+                    the best <a href="https://www.etek.com.bd/pos-printer">POS printers</a> at an affordable
+                    price. Our printers are known
+                    for their quality and durability. We have a wide range of printers to choose from, so you can find
+                    the perfect one for your needs.
+                </p>
+                <h2>Best Barcode Printer &amp; Scanner shop at an affordable price in Bangladesh</h2>
+                <p>
+                    In this fast-paced world, a barcode printer &amp; scanner is a must-have for any business. At our
+                    shop, we offer the best barcode printer &amp; scanner at an affordable price in Bangladesh. We have
+                    a wide range of barcode
+                    printers &amp; scanners to choose from, so you can find the perfect one for your business. We also
+                    offer a wide range of accessories, so you can get everything you need to get started.
+                </p>
+                <h2>Best Computer Monitor shop at an affordable price in Bangladesh</h2>
+                <p>
+                    We are the best <a href="https://www.etek.com.bd/monitor">computer monito</a>r shop at an
+                    affordable price in Bangladesh. With years of experience in the industry, we provide top-quality
+                    products and services to our
+                    customers. Our <strong>monitors</strong> are perfect for <a
+                        href="https://www.etek.com.bd/hp">gaming</a>, <a
+                        href="https://www.etek.com.bd/asus-monitor-price">video editing</a>, and graphic design.
+                    We also
+                    offer a wide variety of other<a href="https://www.etek.com.bd/peripheral"> computer
+                        accessories</a>. Shop now.
+                </p>
+            </div>
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+
+}
+</script>
