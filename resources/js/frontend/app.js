c631e00c020aae1b505c794050b2b148236a9940
Md-shefat-masum <mshefat924@gmail.com>
2024-06-10 22:05:34 +0600

    website js update

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from "vue";
-import { createInertiaApp, Link } from "@inertiajs/vue3";
+import { createInertiaApp, Head, Link } from "@inertiajs/vue3";
 createInertiaApp({
+    // title: title => title ? `${title} - Ping CRM` : 'Ping CRM',
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
         return pages[`./Pages/${name}.vue`];
@@ -9,6 +10,7 @@ createInertiaApp({
         const app = createApp({ render: () => h(App, props) });
         app.use(plugin);
         app.component("Link", Link);
+        app.component('Head', Head);
         app.mount(el);
     },
 });
