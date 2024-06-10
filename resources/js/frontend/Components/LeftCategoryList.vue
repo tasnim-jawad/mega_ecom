0aeadd96d31e4e15358b7c13e67f0a03d796371f
Md-shefat-masum <mshefat924@gmail.com>
2024-06-10 22:07:24 +0600

    dynamic nav categories

---
+++ resources/js/frontend/Components/LeftCategoryList.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul class="category_list">
+        <li v-for="category in top_categories" :key="category.id">
+            <Link :href="`/category/${category.slug}`">
+                <img :src="category.image" :alt="category.title">
+                <span class="link_title">
+                    {{ category.title }}
+                </span>
+            </Link>
+        </li>
+    </ul>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+    data: () => ({
+        top_categories: [],
+    }),
+    created: function () {
+        this.get_categories();
+    },
+    methods: {
+        get_categories: async function () {
+            let res = await axios.get('/api/v1/nav-categories');
+            let data = res.data;
+            this.top_categories = data;
+        },
+    }
+}
+</script>
+<style lang="">
+
+</style>
