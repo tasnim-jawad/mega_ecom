d577129717ae4132ad735e96d7501de906b98105
Md-shefat-masum <mshefat924@gmail.com>
2024-06-25 09:57:07 +0600

    brand list dynamci

--- resources/js/frontend/Pages/Home/Sections/Brands.vue
+++ resources/js/frontend/Pages/Home/Sections/Brands.vue
@@ -3,31 +3,38 @@
         <div class="custom-container">
             <h2 class="title">Brands We Are Working With</h2>
             <ul class="brand_items">
-                <li v-for="item in brands" :key="item">
-                    <img :src="item" alt="">
+                <li v-for="item in brands" :key="item.id">
+                    <img :src="check_image_url(item.image)" :alt="item.title">
                 </li>
             </ul>
         </div>
     </section>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
     data: () => ({
-        brands: [
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/CVo1G2QVpVtSoZlmCvC0l4hL9WIiWLlTQf6XclMC.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/ecb0DAeSHXtbC4Fn6WYz8MBpJuqVeJweFHpO5Tdv.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/SE5RmXlnnn7IGCueKcCC3bSb6VMbpRKCQ9Uj73im.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/psvYUjmOREXrnLNH4O3h9pC3btmXDsyh96vSPEtk.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/igAkXQNWqVIiTSwKVsoG9DmihosVVTL6vp94K6qf.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/8epyndbgfapwj0RYIU5mMDUk88yFYWjffqLOQMFD.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/rCfpiBkUtiOz8Ipakex02EA8uNYXGySvQWH9phRx.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/rkIzWJlmmr3Ae5oJMwgHbFp3Kg9uinm5su2hmHoH.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/hOcd5BOA7MVZ4y4potvCzMypnVKospMJsPZb1aPU.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/p1cHVKZSlCy5VrGjQRcHQclukEoRhapNQQHXz522.webp",
-            "https://demo.activeitzone.com/ecommerce/public/uploads/all/ED7vbI8nnpPeKv6LXGYsGMCOLKG5s5uWyedONBxj.webp",
-            // "https://demo.activeitzone.com/ecommerce/public/uploads/all/EAB1zQHsBz4bs8Z4DSKmdqYVV2OReXMxiqp32hCk.webp"
-        ]
-
-    })
+        brands: []
+    }),
+    created: function(){
+        this.get_brands();
+    },
+    methods: {
+        get_brands: function(){
+            axios.get('/api/v1/brands')
+                .then(res=>{
+                    this.brands = res.data;
+                })
+        },
+        check_image_url: function (url) {
+            try {
+                new URL(url);
+                return url;
+            } catch (e) {
+                return "/"+url;
+            }
+        },
+    }
 }
 </script>
